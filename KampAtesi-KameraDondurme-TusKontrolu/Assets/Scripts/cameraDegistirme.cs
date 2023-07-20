3c60edfe97748b63b7eeea8c5080b4867ab25772
Abdulkadir DURMAZ <abdulkadirdurmaz52@gmail.com>
2023-07-20 16:37:59 +0300

    Unity'de temel tuş kombinasyonları
    Unity ile kaplama nasıl eklenir, klavye girdileri nasıl yentilir ve kamera klavye ile nasıl kontrol edilir sorularına başlangıç seviyesinde cevaplar buldum.

---
+++ KampAtesi-KameraDondurme-TusKontrolu/Assets/Scripts/cameraDegistirme.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraDegistirme : MonoBehaviour
+{
+    public Camera camera2;
+    public Camera camera1;
+    public bool c1mi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera1 = Camera.main;
+        camera2 = GameObject.Find("camera2").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            camera2.enabled = c1mi;
+            camera1.enabled = !c1mi;
+            c1mi = !c1mi;
+        }
+
+        if (c1mi)
+        {
+            camera1.enabled = true;
+            camera2.enabled = false;
+        }
+
+        else
+        {
+            camera1.enabled = false;
+            camera2.enabled = true;
+        }
+    }
+}
---
+++ KampAtesi-KameraDondurme-TusKontrolu/Assets/Scripts/textScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textScript : MonoBehaviour
+{
+    GameObject plane;
+    Text txt;
+    GameObject g;
+    private Canvas cn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plane = GameObject.Find("Plane");
+        cn = GetComponent<Canvas>();
+        txt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          //  Instantiate(plane, transform.position, transform.rotation);
+            for (float i = 255; i >= 0; i -= Time.time)
+            {
+                Debug.Log(i);
+                txt.color = new Color(1, 1, 1, i);
+            }
+        }
+
+        
+    }
+    //void ontriggerenter(Collision other)
+    //{
+    //    if (other.name== "emptygameobject")
+    //    {
+    //        int score;
+    //        score++;
+    //    }
+    //}
+}
